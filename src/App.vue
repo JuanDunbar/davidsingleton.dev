3213b0160bd18f6e2aff89d46edf5225a9173142
David Singleton <dsingleton@mailgun.com>
2022-01-18 22:56:04 -0500

    added particles background

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Particles
+      id="background"
+      :options="options"
+  />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import options from './particle_options'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  setup() {
+    return {
+      options,
+    }
   }
 }
 </script>
